e9aadd99216f7f5143f53ceff3f7595dbe3a72e9
gukai <gukai@bilibili.com>
2018-01-02 11:26:31 +0800

    update

--- bin/client.php
+++ bin/client.php
@@ -8,6 +8,8 @@ $socket = new Thrift\Transport\TSocket('127.0.0.1', 8091);
 $transport = new Thrift\Transport\TFramedTransport($socket);
 $protocol = new Thrift\Protocol\TBinaryProtocol($transport);
 $transport->open();
+$socket->setRecvTimeout(10000);
+$socket->setSendTimeout(10000);
 
 $client = new Authority\AuthorityServiceClient($protocol);
 $ret = $client->getAssignableGroup(1);
--- config.develop/swoole.php
+++ config.develop/swoole.php
@@ -9,4 +9,7 @@ return [
     'package_length_offset' => 0,       // 第N个字节是包长度的值
     'package_body_offset' => 4,         // 从第几个字节计算长度
     // 'daemonize' => 1,
+    'heartbeat_idle_time' => 600,       // 一个连接如果600秒内未向服务器发送任何数据，此连接将被强制关闭
+    'heartbeat_check_interval' => 60,   // 表示每60秒遍历一次
+    'reload_async' => true,             // 将启用异步安全重启特性，Worker进程会等待异步事件完成后再退出
 ];

82117df3a453d4cf9a4df7c2cc92a1f582d7ee1c
gukai <gukai@bilibili.com>
2018-01-02 14:28:13 +0800

    update config/swoole

--- config.develop/swoole.php
+++ config.develop/swoole.php
@@ -3,6 +3,7 @@
 return [
     'worker_num' => 8,
     'dispatch_mode' => 1,               // 1-轮询 3-抢占
+    'open_tcp_keepalive' => 1,          // TCP中有一个Keep-Alive的机制可以检测死连接
     'open_length_check' => true,        // 打开包长检测
     'package_max_length' => 8192000,    // 最大的请求包长度,8M
     'package_length_type' => 'N',       // 长度的类型，参见PHP的pack函数
